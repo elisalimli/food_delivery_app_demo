ed8642f8507d260be3aa4c27f39b08f783d02371
Ali Salimli <alisalimli1899@gmail.com>
2022-07-26 12:05:52 +0400

    navigating to the order delivery screen

--- src/components/RestuarantScreen/RestuarantBotomCard./RBCOrderButton.tsx
+++ src/components/RestuarantScreen/RestuarantBotomCard./RBCOrderButton.tsx
@@ -1,10 +1,15 @@
+import { useNavigation } from "@react-navigation/native";
 import * as React from "react";
 import { Text, TouchableOpacity } from "react-native";
+import { RootStackNavigationProps } from "../../../Screens";
 import { tw } from "../../../utils";
 
 const RBCOrderButton = () => {
+  const navigation = useNavigation<RootStackNavigationProps>();
+
   return (
     <TouchableOpacity
+      onPress={() => navigation.navigate("OrderDelivery")}
       style={tw`bg-primary p-4 items-center justify-center rounded-2xl mt-1`}
     >
       <Text style={tw`text-white font-medium text-lg leading-tight`}>
